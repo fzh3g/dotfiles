a77c65866eedd300a468c77630394fb84f597a5e
zhengfaxiang <fxzheng0906@outlook.com>
2015-06-19 03:57:27 +0800

    deleted useless items

--- stow_install.sh
+++ stow_install.sh
@@ -17,11 +17,8 @@ declare -a pkgs=(
 
 for dir in "${pkgs[@]}"
 do
-    if [ $dir != images ]; then {
-        echo -e "\e[1;36m==>\e[0m \e[1;31mstowing $dir\e[0m"
-        stow -R $dir
-    }
-    fi
+    echo -e "\e[1;36m==>\e[0m \e[1;31mstowing $dir\e[0m"
+    stow -R $dir
 done
 
 echo -e "\e[3;93mStow finished! Enjoy your journey!\e[0m"
