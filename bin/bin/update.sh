075117db216658912880b127e3a0c0da7df012d0
Faxiang Zheng <fxzheng0906@outlook.com>
2016-07-25 21:49:45 +0800

    update.sh

--- bin/bin/update.sh
+++ bin/bin/update.sh
@@ -1,6 +1,26 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 echo -e "\e[38;5;226mStarting update...\e[0m"
+
+# pacman && yaourt
+echo -e "\e[38;5;164m==>\e[0m \e[38;5;85mUpdating system packages...\e[0m"
+sudo pacman -Syu
+yaourt -Syu
+
+# pip
+echo -e "\e[38;5;164m==>\e[0m \e[38;5;85mUpdating Python packages...\e[0m"
+pip freeze | grep -v '^\-e' | cut -d = -f 1  | xargs sudo pip install -U
+
+# ruby gems
+echo -e "\e[38;5;164m==>\e[0m \e[38;5;85mUpdating Ruby gems...\e[0m"
+gem update
+gem cleanup
+
+# npm
+echo -e "\e[38;5;164m==>\e[0m \e[38;5;85mUpdating npm...\e[0m"
+npm -g update
+npm ddp -g
+cd ~/Web/blog/ && ncu -u && npm install && npm ddp
 
 # oh-my-zsh
 echo -e "\e[38;5;164m==>\e[0m \e[38;5;85mUpdating oh-my-zsh...\e[0m"
@@ -28,9 +48,33 @@ else
         git pull origin master
 fi
 
-# ruby gems
-echo -e "\e[38;5;164m==>\e[0m \e[38;5;85mUpdating Ruby gems\e[0m"
-gem update
-gem cleanup
+# enhancd
+echo -e "\e[38;5;164m==>\e[0m \e[38;5;85mUpdating Enhancd...\e[0m"
+if [ ! -d ~/.enhancd/ ]; then
+    git clone https://github.com/b4b4r07/enhancd ~/.enhancd
+else
+    cd ~/.enhancd/ && git pull origin master
+fi
+source ~/.enhancd/init.sh
 
+# fzf
+echo -e "\e[38;5;164m==>\e[0m \e[38;5;85mUpdating fzf...\e[0m"
+if [ ! -d ~/.fzf/ ]; then
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+else
+    cd ~/.fzf/ && git pull origin master
+fi
+~/.fzf/install --all
+rm ~/.bashrc ~/.fzf.bash
+
+# IDL
+echo -e "\e[38;5;164m==>\e[0m \e[38;5;85mUpdating IDL libraries...\e[0m"
+cd ~/lib/idl/astrolib/ && git pull origin master
+cd ~/lib/idl/idl-coyote/ && git pull origin master
+
+# source .zshrc
+echo -e "\e[38;5;164m==>\e[0m \e[38;5;85mSource ~/.zshrc\e[0m"
+source ~/.zshrc
+
+# finished!
 echo -e "\e[38;5;226mUpdating finished!\e[0m"
